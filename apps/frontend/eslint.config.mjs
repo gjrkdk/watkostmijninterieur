a7450ff3aaf9911f8c33e5983142b8d121a5ad80
GJRKZB <89792219+GJRKZB@users.noreply.github.com>
2024-12-21 11:07:41 +0100

    configure ESLint, and update Prettier settings

---
+++ apps/frontend/eslint.config.mjs
@@ -0,0 +1,33 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    ignores: ["node_modules", "dist", "build", "coverage"],
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
