df910f5965c74b031b2e05a836274fda8a0893b5
GJRKZB <89792219+GJRKZB@users.noreply.github.com>
2025-01-16 22:12:57 +0100

    use name (firstName) variable  in confirmation

--- apps/frontend/src/components/form/Confirmation/Confirmation.tsx
+++ apps/frontend/src/components/form/Confirmation/Confirmation.tsx
@@ -1,10 +1,14 @@
 import { Box, Typography } from "@mui/material";
+import { useFormContext } from "../../../context/FormContext";
 
 export const Confirmation = () => {
+  const { contactDetails } = useFormContext();
+
+  const name = contactDetails?.firstName || "";
+
   return (
     <Box>
-      <Typography variant="h1">Thank you for your request!</Typography>
-      {/* TODO: Create and add Summary component */}
+      <Typography variant="h1">{`Thank you ${name}, for submitting the form!`}</Typography>
       <Typography variant="body1">You will receive an email with the estimations.</Typography>
     </Box>
   );
