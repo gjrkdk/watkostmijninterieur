fb62856ae6728e50e14c1b93dc59ed35ce57b16e
Robin Konijnendijk <89792219+gjrkdk@users.noreply.github.com>
2025-02-20 23:58:25 +0100

    remove logs

--- apps/frontend/src/context/FormProvider.tsx
+++ apps/frontend/src/context/FormProvider.tsx
@@ -72,8 +72,6 @@ export const FormProvider: React.FC<IFormContext> = ({ children }) => {
     })),
   });
 
-  console.log(selectedFormValues);
-
   const [contactDetails, setContactDetails] = useState<IContactDetails>({
     // firstName: "",
     // email: "",
