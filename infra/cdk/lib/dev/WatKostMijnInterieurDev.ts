a487047b43e13e77497def728c283a46e7b8d97c
gjrkdk <89792219+GJRKZB@users.noreply.github.com>
2025-03-13 20:56:33 +0100

    update stack to handle options preflight

--- infra/cdk/lib/dev/WatKostMijnInterieurDev.ts
+++ infra/cdk/lib/dev/WatKostMijnInterieurDev.ts
@@ -1,41 +1,62 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import { Table, AttributeType, BillingMode } from "aws-cdk-lib/aws-dynamodb";
 import * as path from "path";
 import * as apigatewayv2 from "aws-cdk-lib/aws-apigatewayv2";
 import * as integrations from "aws-cdk-lib/aws-apigatewayv2-integrations";
-import { CfnOutput } from "aws-cdk-lib";
+// import { CfnOutput } from "aws-cdk-lib";
 
 export class WatKostMijnInterieurDev extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const helloLambdaFn = new lambda.Function(this, "StageEnvironmentFunction", {
+    const stageName = "dev";
+    const tableName = process.env.TABLE_NAME_DEV || "Default";
+
+    const contactsTable = new Table(this, "contactsTable", {
+      tableName: tableName,
+      partitionKey: { name: "id", type: AttributeType.STRING },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const priceCalculation = new lambda.Function(this, "PriceCalculationFunction", {
       runtime: lambda.Runtime.NODEJS_LATEST,
       handler: "lambdaHandler.handler",
       code: lambda.Code.fromAsset(path.join(__dirname, "../../../../apps/backend/dist")),
       environment: {
         ENV: "dev",
+        tableName: contactsTable.tableName,
       },
     });
 
+    contactsTable.grantWriteData(priceCalculation);
+
     const httpApi = new apigatewayv2.HttpApi(this, "HttpApi", {
-      apiName: "Hello API",
+      apiName: "PriceCalculationService",
       corsPreflight: {
         allowOrigins: ["*"],
-        allowMethods: [apigatewayv2.CorsHttpMethod.GET, apigatewayv2.CorsHttpMethod.POST],
+        allowMethods: [
+          apigatewayv2.CorsHttpMethod.GET,
+          apigatewayv2.CorsHttpMethod.POST,
+          apigatewayv2.CorsHttpMethod.OPTIONS,
+        ],
         allowHeaders: ["Content-Type", "Authorization"],
       },
     });
 
     httpApi.addRoutes({
-      path: "/hello",
-      methods: [apigatewayv2.HttpMethod.GET],
-      integration: new integrations.HttpLambdaIntegration("LambdaIntegration", helloLambdaFn),
+      path: "/price-calculation",
+      methods: [apigatewayv2.HttpMethod.GET, apigatewayv2.HttpMethod.POST],
+      integration: new integrations.HttpLambdaIntegration("LambdaIntegration", priceCalculation),
     });
 
-    new CfnOutput(this, "ApiEndpoint", {
-      value: httpApi.apiEndpoint,
+    const apiStage = new apigatewayv2.CfnStage(this, "ApiStage", {
+      apiId: httpApi.httpApiId,
+      stageName: stageName,
+      autoDeploy: true,
     });
+    apiStage.node.addDependency(httpApi);
   }
 }
