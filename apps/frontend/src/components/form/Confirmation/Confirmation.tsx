ff7e9f2e745df76d7890fb833d387f125aec6fea
gjrkdk <89792219+GJRKZB@users.noreply.github.com>
2025-03-13 18:39:25 +0100

    get price indication from Lambda and display on frontend

--- apps/frontend/src/components/form/Confirmation/Confirmation.tsx
+++ apps/frontend/src/components/form/Confirmation/Confirmation.tsx
@@ -2,7 +2,7 @@ import { Box, Typography } from "@mui/material";
 import { useFormContext } from "../../../context/FormContext";
 
 export const Confirmation = () => {
-  const { contactDetails } = useFormContext();
+  const { contactDetails, response } = useFormContext();
 
   const name = contactDetails?.firstName || "";
 
@@ -10,6 +10,11 @@ export const Confirmation = () => {
     <Box>
       <Typography variant="h1">{`Thank you ${name}, for submitting the form!`}</Typography>
       <Typography variant="body1">You will receive an email with the estimations.</Typography>
+      {response && (
+        <Box>
+          <Typography>{JSON.stringify(response)}</Typography>
+        </Box>
+      )}
     </Box>
   );
 };
